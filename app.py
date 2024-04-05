cddf35fab9108567903bbd6e5312891435273804
Shruti <126263858+Shrut0025@users.noreply.github.com>
2024-04-05 11:03:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import string
 import nltk
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
